b6c09582259af2a32568fd61a9c9256d0d8f3f3e
George Hutchings <george.hutchings@18@imperial.ac.uk>
2023-03-21 15:16:42 +0000

    neaten code

--- time-series-code.py
+++ time-series-code.py
@@ -22,9 +22,8 @@ class cd:
 
     def __exit__(self, etype, value, traceback):
         os.chdir(self.savedPath)
-
+############################## read data #####################################
 #with cd("Documents/PhD-year-1/time-series-course/time_series-walden"):
-# read data
 with open('SDM_3.dat') as f:
     first_line = f.readline().strip('\n').split(' ')
     r, B, K, nf = np.loadtxt(first_line, unpack=True)
@@ -35,7 +34,6 @@ r, K, nf = int(r), int(K), int(nf)
 def f(i1, i2):
     return np.array([float(x) for x in lines[i1:i2]])
 frequencies = f(1, nf+1)
-
 
 Sf = np.zeros((r,r,nf), dtype=complex) 
 idx_start = nf+2 #starting index
@@ -52,28 +50,10 @@ for r_idx in range((r+1)*r//2):
     Sf[i-1, j-1, :] = real + 1j*im
     if i!=j:
         Sf[j-1, i-1,:] = Sf[i-1, j-1, :]
+ ##############################################################################       
 
-invSf = np.empty_like(Sf)
-for i in range(nf):
-    invSf[:,:,i] = np.linalg.inv(Sf[:,:,i])
-
-
-
-jks2 = tuple([ (j, k) for j,k in jks if j!=k]) #j,k without j,j
-gammaf = {'columns': jks2} #freq = row, (j,k) = col
-gammaf['rows'] = frequencies
-
-tmp = np.empty((nf, len(jks2)))
-for i, jk in enumerate(jks2):
-    j, k = jk
-    foo = (abs(invSf[j-1,k-1,:])**2)/ (invSf[j-1,j-1,:]*invSf[k-1,k-1,:]) 
-    #assert(np.max(abs(foo.imag))<0.0001)
-    tmp[:,i] = foo
-gammaf['gamma'] = tmp
-
+#c## alculate indexes if f' values
 assert(np.all(np.sort(frequencies) == frequencies))
-
-
 f_prime_idx = [np.argmax(frequencies >= B/2)]
 for i, freq, in enumerate(frequencies):
     tmp = f_prime_idx[-1]
@@ -85,6 +65,26 @@ for i, freq, in enumerate(frequencies):
             f_prime_idx += [ tmp2 ]
 f_prime_idx = np.array(f_prime_idx)
 
+### calculate S^-1(f) for all f given
+invSf = np.empty_like(Sf)
+for i in range(nf):
+    invSf[:,:,i] = np.linalg.inv(Sf[:,:,i])
+
+
+# calculate gamma(f) for all f given
+jks2 = tuple([ (j, k) for j,k in jks if j!=k]) #j,k without j,j
+gammaf = {'columns': jks2} #freq = row, (j,k) = col
+gammaf['rows'] = frequencies
+tmp = np.empty((nf, len(jks2)))
+for i, jk in enumerate(jks2):
+    j, k = jk
+    num = invSf[j-1,k-1,:] * invSf[j-1,k-1,:].conj()
+    denom = invSf[j-1,j-1,:]*invSf[k-1,k-1,:]
+    #assert(np.max(abs((num/denom).imag))<0.0001)
+    tmp[:,i] = num/denom
+gammaf['gamma'] = tmp
+
+# selecting nly the fa (independent) needed
 gammaf['gammafa'] = gammaf['gamma'][f_prime_idx, :]
 
 W = -2*K* np.sum(np.log(gammaf['gammafa']), axis=0)
@@ -100,9 +100,14 @@ scale = 1/beta
 L = (r**2-r)//2
 Ci = np.arange(1, L+1)
 alpha_hypothesis = 0.01
-Ci = gamma.ppf((1-alpha_hypothesis/Ci), A, scale=scale)
+Ci = gamma.ppf((1-(alpha_hypothesis/Ci)), A, scale=scale)
 
-print(Ci > W)
+print(Ci > W) #TODO all fales?!
+
+print(Ci)
+print('='*40)
+print(W)
+
  
     
     
